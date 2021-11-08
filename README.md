0be15451558dfe2641eedd092b00d1c47c4b9d7d
Sam <sam@localseoguide.com>
2021-11-08 17:28:40 -0600

    Built automated voting functionality

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # LSG Vote Troller
 This is a simple selenium based script to blast voting pages. 
+
+## Config
+Create a file called `config.py`. It should look like this example, but replace the values with your own.
+
+```python
+"""Configuration file."""
+
+# The URL of the poll
+poll_url = 'https://www.example.com/poll'
+
+# The names of the nominees to vote for
+# These should match the names listed on the poll exactly
+names = [
+    'Captain SEO',
+    'John Smith',
+    'Jane Doe',
+    'Mr Nobody'
+]
+
+# The proxy connection strings
+http_proxy_string = 'https://username:password@your.proxyprovider.com:<port>'
+https_proxy_string = 'https://username:password@your.proxyprovider.com:<port>'
+
+# The selector of the root element that contains the name of the nominee and
+# the button to click to vote for them.
+# Note that "NAME" here will be replaced with a name from your list of names
+# (see above)
+root_element_xpath = (
+    '//div[contains(@class, "seo_expert-item")][contains(., "NAME")]'
+)
+
+# The sub-xpath that specifies the button to click to vote for the nominee
+# This xpath will be applied from within the root_element_xpath
+button_xpath = '//*[contains(@class, "likebtn-button")]'
+
+# Number of times to vote
+num_of_votes = 50
+```
--- vote-troller.py
+++ vote-troller.py
@@ -1,21 +1,75 @@
-import selinium
-import selinium-wire
+"""The automated vote trolling module."""
 
-# provide targets
-list_of_names = ['Rejoice Ojiaku', 'Wilhemina Davis', 'Aimee Sanford', 'Rachel Anderson', 'Alison Enzinna',
-                 'Bria Poullard', 'Caleb Romo', 'Jess Peck', 'Bryan Heckler', 'Tessa Voecks']
+import config
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from seleniumwire import webdriver
+from chromedriver_py import binary_path
 
-# provide element/attribute (text) to find + element/attribute to click
-target_elements = {'root_ele':  {
-    'class': 'seo_expert-item'
-},
-    'click_target': {
-        'onclick':'LikeBtn.vote'
+
+# Web driver options
+options = {
+    'proxy': {
+        'http': config.http_proxy_string,
+        'https': config.https_proxy_string,
+        'no_proxy': ''
     }
 }
 
 
-Class VoteTroller:
-    def __init__(self):
-        print('Testing')
+def vote_for_names(driver, names):
+    """Take a list of names and vote for each one."""
+    for name in names:
+        # Find the root element with the name
+        root_el = driver.find_element(
+            By.XPATH, config.root_element_xpath.replace('NAME', name)
+        )
 
+        # Get the root element location
+        location = root_el.location
+
+        # Scroll to the element
+        driver.execute_script(
+            """
+            window.scrollTo(
+              {
+                top: arguments[0],
+                left: 0,
+                behavior: 'instant'
+              }
+            );
+            """,
+            location['y']
+        )
+
+        # Find the button
+        button_el = driver.find_element(
+            By.XPATH,
+            config.root_element_xpath.replace('NAME', name) +
+            config.button_xpath
+        )
+
+        # Click it
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable(button_el)
+        ).click()
+
+
+# Loop the specified number of times
+for i in range(0, config.num_of_votes):
+    # Create the webdriver
+    driver = webdriver.Chrome(
+        seleniumwire_options=options,
+        executable_path=binary_path
+    )
+    driver.implicitly_wait(10)
+
+    # Visit the URL of the poll
+    driver.get(config.poll_url)
+
+    # Vote for the names
+    vote_for_names(driver, config.names)
+
+    # Close the web driver
+    driver.close()
